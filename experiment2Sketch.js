2dc1513b5e38cb7208a93cb926b39ed1b1f63404
Khushboo <khushbooatwork30@gmail.com>
2024-12-06 14:07:06 +0530

    add more changes in this file

--- experiment2Sketch.js
+++ experiment2Sketch.js
@@ -1,7 +1,22 @@
+ 
 
-function setup(){
-    createCanvas(1800,900)
- }
-function draw(){
-    background("pink")
+let rectY = 400; // Initial Y position of the rectangle
+let speed = 5; // Speed of the rectangle
+
+function setup() {
+  createCanvas(1800, 900); // Create a canvas of 400x400
 }
+
+function draw() {
+  background("pink"); // Clear the background
+  
+  fill(100, 200, 255); // Set rectangle color
+  rect(900, rectY, 100, 50); // Draw the rectangle (x, y, width, height)
+
+  rectY -= speed; // Move the rectangle upword
+
+  // If the rectangle moves beyond the canvas height, reset to the top
+  if ( rectY<0)    {
+    rectY = height;
+  }
+}   

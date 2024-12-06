73bed876716c561e5fa70e67ddf4928d480743b0
Khushboo <khushbooatwork30@gmail.com>
2024-12-06 13:44:54 +0530

    make a canvas in experiment2Sketch2.js file

--- experiment2Sketch.js
+++ experiment2Sketch.js
@@ -1,7 +1,7 @@
-// function draw() {
-// //     //{!1} A normal distribution with mean 320 and standard deviation 60
-// //     let x = randomGaussian(320, 60);
-// //     noStroke();
-// //     fill(0, 10);
-// //     circle(x, 120, 16);
-// //   }
+
+function setup(){
+    createCanvas(1800,900)
+ }
+function draw(){
+    background("pink")
+}

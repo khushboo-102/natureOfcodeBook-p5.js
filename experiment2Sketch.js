03cd5311f70954c54494b343863c80ac2701567a
Khushboo <khushbooatwork30@gmail.com>
2024-12-06 14:26:13 +0530

    add heading

--- experiment2Sketch.js
+++ experiment2Sketch.js
@@ -1,6 +1,6 @@
  
 
-let rectX = 0; // Initial Y position of the rectangle
+let rectX = 0; // Initial X position of the rectangle
 let speed = 5; // Speed of the rectangle
 
 function setup() {
@@ -16,13 +16,14 @@ function draw() {
   rectX += speed; // Move the rectangle rightside word
 
   // If the rectangle moves beyond the canvas height, reset to the top
-  if ( rectX>width)    {
+  if ( rectX>width){
     rectX = 0;
   }
+
+//Heading
+textSize(40)
+// fill("pink")
+stroke(178, 102, 255)
+text("Moving rectangle right side" ,700,90)
 }  
 
-// //Heading
-// textSize(30)
-// // fill("pink")
-// stroke("pink")
-// text("move rectangle right side" ,600,200)
